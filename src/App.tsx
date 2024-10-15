752c9ca66ca84555d75f8c9164afc12b3c38177f
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-15 20:55:30 +0200

    wip build ingame board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import menuIcon from "./assets/images/icon-menu.svg";
 import IconButton from "./components/icon-button";
 import heartIcon from "./assets/images/icon-heart.svg";
+import { cn } from "./assets/lib/utils";
+import { alphabet } from "./assets/lib/const";
+
+const searchPhrase = "United Kingdom";
+const searchPhraseWordArray = searchPhrase.split(" ");
 
 function App() {
   return (
@@ -9,8 +14,8 @@ function App() {
       {/* <MainMenuBoard /> */}
       {/* <HowToPlayBoard /> */}
       {/* <CategoryPickBoard /> */}
-      <section className="grid w-full max-w-[324px] gap-24 py-4">
-        <header className="flex items-center justify-between">
+      <section className="grid w-full max-w-[324px] gap-20 py-4">
+        <header className="flex w-full items-center justify-between">
           <div className="flex items-center gap-4">
             <IconButton icon={menuIcon} />
             <h1 className="text-4xl tracking-wide">Countries</h1>
@@ -22,6 +27,38 @@ function App() {
             <img src={heartIcon} alt="heart icon" className="size-7" />
           </div>
         </header>
+        {/* Search phrase */}
+        <section className="flex flex-wrap justify-center gap-y-4">
+          {searchPhraseWordArray.map((word, wordIndex) => (
+            <section key={wordIndex} className="flex gap-x-3">
+              {word.split("").map((letter, letterIndex) => {
+                const shouldHide = Math.random() < 0.5; // 50% chance to hide the letter
+                return (
+                  <span
+                    key={letterIndex}
+                    className={cn(
+                      "bg-blue/100 shadow-custom flex h-16 w-9 items-center justify-center overflow-hidden rounded-xl text-4xl",
+                      shouldHide && "bg-blue/40 text-transparent shadow-none",
+                    )}
+                  >
+                    {letter.toUpperCase()}
+                  </span>
+                );
+              })}
+            </section>
+          ))}
+        </section>
+        {/* Keayboard */}
+        <section className="mt-10 flex flex-wrap justify-center gap-x-2 gap-y-4">
+          {alphabet.map((letter, index) => (
+            <button
+              key={index}
+              className="text-dark-navy flex items-center justify-center rounded-xl bg-white px-3 py-3 text-2xl"
+            >
+              {letter}
+            </button>
+          ))}
+        </section>
       </section>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,11 @@ export default {
         'mobile-background': "url('./src/assets/images/background-mobile.svg')",
         'pink-gradient': `linear-gradient(#fe71fe, #2462ff)`,
       },
-      
+      boxShadow: {
+        // Add inner shadow with position X: 0, Y: 0, blur: 0, spread: 6, color: #CFD8DC
+        'custom': 'inset 0 4px 0 3px #3c74ff, 0 2px 0 3px #140e66'
+      },
+      //
     },
   },
   plugins: [],
