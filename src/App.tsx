1342d220607e94cbc10c661354c563f1ab6a1e07
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-15 17:20:45 +0200

    Create category pick board as separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 // import MainMenuBoard from "./components/main-menu-board";
-import HeaderWrapper from "./components/header-wrapper";
-import Heading from "./components/heading";
+
+import CategoryPickBoard from "./components/category-pick-board";
+
 // import HowToPlayBoard from "./components/how-to-play-board";
-import IconButton from "./components/icon-button";
-import backIcon from "./assets/images/icon-back.svg";
-import Button from "./components/button";
 
 function App() {
   return (
@@ -12,20 +10,7 @@ function App() {
       {/* MAIN MENU */}
       {/* <MainMenuBoard /> */}
       {/* <HowToPlayBoard /> */}
-      <section className="grid w-full max-w-[324px] gap-24 py-4">
-        <HeaderWrapper>
-          <IconButton icon={backIcon} />
-          <Heading>Pick a Category</Heading>
-        </HeaderWrapper>
-        <section className="grid gap-4">
-          <Button className="rounded-2xl py-5 text-2xl">movies</Button>
-          <Button className="rounded-2xl py-5 text-2xl">TV shows</Button>
-          <Button className="rounded-2xl py-5 text-2xl">Countries</Button>
-          <Button className="rounded-2xl py-5 text-2xl">capital cities</Button>
-          <Button className="rounded-2xl py-5 text-2xl">animals</Button>
-          <Button className="rounded-2xl py-5 text-2xl">sports</Button>
-        </section>
-      </section>
+      <CategoryPickBoard />
     </section>
   );
 }
---
+++ src/components/category-pick-board.tsx
@@ -0,0 +1,32 @@
+import HeaderWrapper from "./header-wrapper";
+import IconButton from "./icon-button";
+import backIcon from "../assets/images/icon-back.svg";
+import Button from "./button";
+import Heading from "./heading";
+
+const hangmanCategories = [
+  "movies",
+  "TV shows",
+  "countries",
+  "capital cities",
+  "animals",
+  "sports",
+];
+
+export default function CategoryPickBoard() {
+  return (
+    <section className="grid w-full max-w-[324px] gap-24 py-4">
+      <HeaderWrapper>
+        <IconButton icon={backIcon} />
+        <Heading>Pick a Category</Heading>
+      </HeaderWrapper>
+      <section className="grid gap-4">
+        {hangmanCategories.map((category) => (
+          <Button key={category} className="rounded-2xl py-5 text-2xl">
+            {category}
+          </Button>
+        ))}
+      </section>
+    </section>
+  );
+}
