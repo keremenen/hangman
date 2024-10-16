f623ad723dd0d6ba3bd8ad0f3ad7e802d83470f2
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-16 20:28:24 +0200

    Refactor hidden-letters component

--- src/components/hidden-letters.tsx
+++ src/components/hidden-letters.tsx
@@ -8,31 +8,44 @@ export default function HiddenLetters() {
 
   // Derive the wordArray from the word
   const wordArray = word?.split(" ");
-
-  // Create a global index to keep track of the letters
+  // Create a global index to keep track of the letters (trick to avoid spaces in phrases)
   let globalIndex = 0;
 
   return (
     <section className="flex flex-wrap justify-center gap-x-8 gap-y-4">
       {wordArray?.map((word, wordIndex) => (
-        <section className="flex gap-x-3" key={wordIndex}>
+        <div className="flex gap-x-3" key={wordIndex}>
           {word.split("").map((letter, letterIndex) => {
             const isVisible = visibleLetters[globalIndex];
             globalIndex++;
             return (
-              <span
+              <SingleLetterTile
                 key={letterIndex}
-                className={cn(
-                  "flex h-16 w-9 select-none items-center justify-center overflow-hidden rounded-xl bg-blue/100 text-4xl text-transparent opacity-10 shadow-none",
-                  { "text-white opacity-100": isVisible },
-                )}
-              >
-                {letter}
-              </span>
+                letter={letter}
+                isVisible={isVisible}
+              />
             );
           })}
-        </section>
+        </div>
       ))}
     </section>
+  );
+}
+
+type SingleLetterTileProps = {
+  letter: string;
+  isVisible: boolean;
+};
+
+function SingleLetterTile({ letter, isVisible }: SingleLetterTileProps) {
+  return (
+    <span
+      className={cn(
+        "flex h-16 w-9 select-none items-center justify-center overflow-hidden rounded-xl bg-blue/100 text-4xl text-transparent opacity-10 shadow-none",
+        { "text-white opacity-100": isVisible },
+      )}
+    >
+      {letter}
+    </span>
   );
 }
