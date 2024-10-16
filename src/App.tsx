a014c4a1c29cfed2494e26d44b08567de58a1cc4
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-16 10:52:33 +0200

    Add modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import Button from "./components/button";
-import CategoryPickBoard from "./components/category-pick-board";
-import MainMenuBoard from "./components/main-menu-board";
+import Heading from "./components/heading";
+// import CategoryPickBoard from "./components/category-pick-board";
+// import MainMenuBoard from "./components/main-menu-board";
 
 function App() {
   return (
     <section className="z-10 mx-auto flex min-h-screen items-center justify-center">
       {/* <InGameBoard /> */}
-      <CategoryPickBoard />
-      <MainMenuBoard />
+      {/* <CategoryPickBoard /> */}
+      {/* <MainMenuBoard /> */}
       <section className="absolute flex">
         <div className="menu-box relative flex w-[324px] flex-col items-center py-20">
-          {/* <div className="absolute">
+          <div className="absolute -top-14">
             <Heading className="text-8xl">Paused</Heading>
-          </div> */}
+          </div>
           <div className="flex flex-col items-center gap-[34px]">
             <Button>Continue</Button>
-            <Button variant="secondary">Quit Game</Button>
             <Button>New Category</Button>
+            <Button variant="secondary">Quit Game</Button>
           </div>
         </div>
       </section>
