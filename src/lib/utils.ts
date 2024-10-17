211794ba0a535e254f12a26d59866d72c8818ee5
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-17 13:07:45 +0200

    Remove unused function from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -20,36 +20,6 @@ export const unslugify = (text: string) => {
   if (!text) return "";
   return text.replace(/-/g, " ");
 };
-// export const getAllCategories = (): Categories[] => {
-//   if (!database.categories) return [];
-//   return Object.keys(database.categories) as Categories[];
-// };
-
-// const getRandomWordFromCategory = (category: Categories): string => {
-//   const words = database.categories[category];
-//   if (!words || words.length === 0) {
-//     throw new Error(`Category ${category} not found or empty`);
-//   }
-
-//   const randomIndex = Math.floor(Math.random() * words.length);
-//   return words[randomIndex].name;
-// };
-
-// export const handleCategoryChoice = (category: CategoryKeys) => {
-//   const setNewCategory = useGameStore.getState().setNewCategory;
-//   const setNewWord = useGameStore.getState().setNewWord;
-//   const setVisibleLetters = useGameStore.getState().setVisibleLetters;
-
-//   const newWord = getRandomWordFromCategory(category);
-//   setNewCategory(category);
-//   setNewWord(newWord);
-//   setVisibleLetters(newWord);
-// };
-
-export const cutWord = (word: string | null) => {
-  if (!word) return [];
-  return word.split(" ").join("").split("");
-};
 
 export const getCategoryNameFromUrl = (): Categories => {
   const params = new URLSearchParams(window.location.search);
