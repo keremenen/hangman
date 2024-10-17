f7aabed901b4d0bebc670566a46fb97849faea4e
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-17 12:52:38 +0200

    Refactor app-bar component

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,27 +1,24 @@
-import HeaderWrapper from "./header-wrapper";
-import IconButton from "./icon-button";
-import HealthBar from "./health-bar";
-import { useGameStore } from "../stores/gameStore";
 import menuIcon from "../assets/images/icon-menu.svg";
-import PauseModal from "./pause-modal";
+import { useGameStore } from "../stores/gameStore";
+import IconButton from "./icon-button";
+
+// Components
+import HeaderWrapper from "./header-wrapper";
+import HealthBar from "./health-bar";
 
 export default function AppBar() {
   const togglePause = useGameStore((state) => state.togglePause);
   const selectedCategory = useGameStore((state) => state.selectedCategory);
-  const isPaused = useGameStore((state) => state.isPaused);
   const health = useGameStore((state) => state.health);
 
   return (
     <HeaderWrapper>
       <div className="flex items-center gap-4">
         <IconButton icon={menuIcon} onClick={togglePause} className="z-20" />
-        {selectedCategory && (
-          <h1 className="text-4xl capitalize">{selectedCategory}</h1>
-        )}
+        <h1 className="text-4xl capitalize">{selectedCategory}</h1>
       </div>
 
       <HealthBar currentHealth={health} />
-      {isPaused && <PauseModal />}
     </HeaderWrapper>
   );
 }
