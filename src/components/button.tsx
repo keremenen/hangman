749c5639c9af083b3bce1e8c6fee389072c2bfd1
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-11-10 20:05:44 +0100

    Refactor button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,13 +9,14 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 };
 
 const buttonVariants = cva(
-  "flex items-center px-16 text-heading-s uppercase rounded-full h-[62px] whitespace-nowrap disabled:opacity-50 disabled:pointer-events-none transition-color ease-in-out duration-200 ",
+  "flex items-center px-16 text-heading-s uppercase rounded-full h-[62px] whitespace-nowrap disabled:opacity-50 disabled:pointer-events-none transition-color ease-in-out duration-200 focus-visible:outline-none",
   {
     variants: {
       variant: {
-        primary: "bg-blue shadow-primary-button hover:brightness-125",
+        primary:
+          "bg-blue shadow-primary-button hover:bg-[#5B8AFF] focus-visible:bg-[#5B8AFF]",
         secondary:
-          "bg-pink-gradient shadow-secondary-button hover:brightness-125",
+          "bg-pink-gradient shadow-secondary-button hover:brightness-125 focus-visible:brightness-125",
       },
     },
     defaultVariants: {
