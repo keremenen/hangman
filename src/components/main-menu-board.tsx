90e614573909ae65402fa9fee99003778f86d409
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-11-10 16:51:01 +0100

    Make main board responsive

--- src/components/main-menu-board.tsx
+++ src/components/main-menu-board.tsx
@@ -5,12 +5,12 @@ import PlayButton from "./play-button";
 
 export default function MainMenuBoard() {
   return (
-    <section className="flex min-h-screen justify-center pt-[203px]">
-      <div className="relative flex h-[481px] w-[324px] justify-center rounded-[48px] bg-blue-gradient shadow-primary-box">
-        <div className="absolute top-0 mx-auto -mt-20 max-w-[263px]">
+    <section className="flex min-h-screen justify-center pt-[12.6875rem] sm:pt-[19.8125rem]">
+      <div className="relative flex h-[30.0625rem] w-[20.25rem] justify-center rounded-[3rem] bg-blue-gradient shadow-primary-box sm:h-[31.25rem] sm:w-[37rem] sm:rounded-[4.5rem]">
+        <div className="absolute top-0 mx-auto -mt-20 max-w-[16.4375rem] sm:-mt-28 sm:max-w-[23.3125rem]">
           <Logo />
         </div>
-        <div className="mt-[138px] flex flex-col items-center gap-y-[57px]">
+        <div className="mt-[8.625rem] flex flex-col items-center gap-y-[3.5625rem] sm:mt-[8.0625rem]">
           <Link to="/categories">
             <PlayButton />
           </Link>
