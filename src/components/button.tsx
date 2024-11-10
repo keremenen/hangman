9cbefe86ab70e466f2edb46ae5a4fe19ce1c4fb1
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-11-10 17:34:03 +0100

    Refactor button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,43 +5,33 @@ import { ButtonHTMLAttributes } from "react";
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: string;
   className?: string;
-  variant?: "default" | "secondary" | "square";
-  size?: "default" | "full";
+  variant?: "primary" | "secondary";
 };
 
 const buttonVariants = cva(
-  "px-16 py-2 text-heading-s uppercase rounded-full min-h-[62px] whitespace-nowrap disabled:opacity-50 disabled:pointer-events-none transition-color ease-in-out duration-200",
+  "flex items-center px-16 text-heading-s uppercase rounded-full h-[62px] whitespace-nowrap disabled:opacity-50 disabled:pointer-events-none transition-color ease-in-out duration-200 ",
   {
     variants: {
       variant: {
-        default: "bg-blue shadow-primary-button hover:bg-[#5B8AFF]",
+        primary: "bg-blue shadow-primary-button hover:bg-[#5B8AFF]",
         secondary: "bg-pink-gradient shadow-secondary hover:brightness-125",
-        square: "rounded-2xl py-5 text-2xl",
-      },
-      size: {
-        default: "w-auto",
-        full: "w-full",
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: "primary",
     },
   },
 );
 
 export default function Button({
   variant,
-  size,
+
   className,
   children,
   ...props
 }: ButtonProps) {
   return (
-    <button
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    >
+    <button className={cn(buttonVariants({ variant, className }))} {...props}>
       {children}
     </button>
   );
